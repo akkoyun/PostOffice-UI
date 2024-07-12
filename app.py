5ae90adeb229f9c5fe5d3615d3fb458fd649bd0d
Mehmet Günce Akkoyun <akkoyun@me.com>
2024-07-12 07:23:54 +0000

    initial update

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    "id": 1,
+    "title": "Data Analyst",
+    "location": "Bengaluru, India",
+    "salary": "Rs. 10,00,000"
+  },
+  {
+    "id": 2,
+    "title": "Data Scientist",
+    "location": "Delhi, India",
+    "salary": "Rs. 15,00,000"
+  },
+  {
+    "id": 3,
+    "title": "Frontend Engineer",
+    "location": "Remote",
+    "salary": "Rs. 12,00,000"
+  }
+]
+
+
+
 @app.route("/")
 def hello():
-    return "<p>Hello, World!</p>"
+    return render_template("home.html", 
+                           jobs=JOBS,
+                           name='Gunce'
+                          )
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
