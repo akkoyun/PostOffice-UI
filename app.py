75758933649a31950cddcceb379a5c54972d8d1b
Mehmet Günce Akkoyun <akkoyun@me.com>
2024-07-12 15:48:41 +0300

    Initial update

--- app.py
+++ app.py
@@ -32,18 +32,31 @@ def Get_All_Variables():
 					desc(Models.Log.Create_Time)
 				).limit(10).all()
 
+			# Set Badge Classes
+			service_badge_classes = {
+				1: 'badge-primary',
+				2: 'badge-secondary',
+				3: 'badge-success',
+				4: 'badge-danger',
+				5: 'badge-warning',
+				6: 'badge-info',
+				7: 'badge-light',
+				8: 'badge-dark'
+			}
+
+
 
 
 			# Set Data Type List
 			Data_Type_List = [
 				{
-					'Create_Time': Log.Create_Time,
-					'Log_Level_ID': Log.level.Log_Level_Name,
-					'Log_Description_ID': Log.description.Log_Description,
-					'Service_ID': Log.service.Service_Name,
-					'Device_ID': Log.Device_ID,
-					'Log_Message': Log.Log_Message,
-				} for Log in Query_Log
+					'Create_Time': log.Create_Time,
+					'Device_Name': log.device.Device_Name,
+					'Log_Level_Name': log.level.Log_Level_Name,
+					'Service_Name': log.service.Service_Name,
+					'Log_Description_Name': log.description.Log_Description_Name,
+					'Service_Badge_Class': service_badge_classes.get(log.Service_ID, 'badge-primary'),  # Default olarak 'badge-primary'
+				} for log in Query_Log
 			]
 
 			# Get Data Type List
