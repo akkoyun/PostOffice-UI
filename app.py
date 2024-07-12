83161d8a485f9e60e842f6e16f501c77642621c0
Mehmet Günce Akkoyun <akkoyun@me.com>
2024-07-12 13:54:07 +0300

    Initial update

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ sys.path.append('/home/postoffice/PostOffice/src')
 # Import Libraries
 from flask import Flask, render_template
 from Setup import Database, Models
+from sqlalchemy import desc
 from sqlalchemy.exc import SQLAlchemyError
 
 # Create Flask App
@@ -22,16 +23,18 @@ def Get_All_Variables():
 		with Database.DB_Session_Scope() as DB:
 
 			# Query all data types
-			Query_Variables = DB.query(Models.Variable).all()
+			Query_Log = DB.query(Models.Log).order_by(desc(Models.Log.Create_Time)).limit(20).all()
 
 			# Set Data Type List
 			Data_Type_List = [
 				{
-					'Variable_ID': Variable.Variable_ID,
-					'Variable_Description': Variable.Variable_Description,
-					'Variable_Unit': Variable.Variable_Unit,
-					'Segment_ID': Variable.Segment_ID
-				} for Variable in Query_Variables
+					'Log_ID': Log.Log_ID,
+					'Log_Level_ID': Log.Log_Level_ID,
+					'Log_Description_ID': Log.Log_Description_ID,
+					'Service_ID': Log.Service_ID,
+					'Device_ID': Log.Device_ID,
+					'Log_Message': Log.Log_Message,
+				} for Log in Query_Log
 			]
 
 			# Get Data Type List
